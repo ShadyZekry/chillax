c7fb0ebfba2268c5553fc6ddc1dcc5163efc0aba
Shady <ShadyZekry75@gmail.com>
2022-05-09 00:11:54 +0200

    Add UI with pattern to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -37,46 +37,78 @@ class _MyHomePageState extends State<MyHomePage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: AppColors.blue4,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              Dictiornary.hello,
-              style: const TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w500,
-                color: Colors.white,
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                'assets/images/pattern.png',
+                repeat: ImageRepeat.repeat,
+                color: Colors.white.withOpacity(0.15),
+                // colorBlendMode: BlendMode.multiply,
               ),
             ),
-            Expanded(
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      Dictiornary.calledWhat,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(30),
-                      margin: const EdgeInsets.symmetric(vertical: 10),
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadiusDirectional.only(
-                          bottomEnd: Radius.circular(40),
-                          topStart: Radius.circular(40),
+                    const Icon(Icons.chair, color: Colors.white, size: 30),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 20),
+                      child: Text(
+                        Dictiornary.chillax,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w900,
                         ),
-                        color: Color(0xFF3b8b6e),
                       ),
-                      child: const Text('iaskdlf lsadjkl',
-                          style: TextStyle(color: Colors.white)),
                     ),
+                    const Icon(Icons.sms, color: Colors.white, size: 30),
                   ],
                 ),
-              ),
+                Text(
+                  Dictiornary.chillChat,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 13,
+                    fontWeight: FontWeight.w100,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          Dictiornary.calledWhat,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(30),
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadiusDirectional.only(
+                              bottomEnd: Radius.circular(40),
+                              topStart: Radius.circular(40),
+                            ),
+                            color: Color(0xFF3b8b6e),
+                          ),
+                          child: const Text('Xx_NoobMaster69_xX',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -3,15 +3,22 @@ class Dictiornary {
 
   static const Map<String, Map> _translations = {
     CodeStrings.arabicCode: {
+      'chillax': 'تشيلاكس',
+      'chillChat': 'روق...واتكلم',
       'hello': 'مرحبا',
       'calledWhat': 'كيف ترغب انا يناديك الناس؟',
     },
     CodeStrings.englishCode: {
+      'chillax': 'Chillax',
+      'chillChat': 'Chill...and chat',
       'hello': 'Hello',
       'calledWhat': 'What would you like\nto be called in there?',
     },
   };
 
+  static String get chillChat =>
+      _translations[currentLanguage]?['chillChat'] ?? '';
+  static String get chillax => _translations[currentLanguage]?['chillax'] ?? '';
   static String get hello => _translations[currentLanguage]?['hello'] ?? '';
   static String get calledWhat =>
       _translations[currentLanguage]?['calledWhat'] ?? '';
