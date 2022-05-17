56aea0e7e2b8a0abc50572d8eb01cc2d769eaa2c
Shady <ShadyZekry75@gmail.com>
2022-05-17 23:48:38 +0200

    Add message bar decoration

--- lib/ui/chat_screen.dart
+++ lib/ui/chat_screen.dart
@@ -17,35 +17,34 @@ class ChatScreen extends StatelessWidget {
             Expanded(
               child: Column(
                 children: const [
-                  ChatBubble(isMyMessage: true),
-                  ChatBubble(isMyMessage: false),
+            _buildBottomAppBar()
                 ],
               ),
             ),
-            Container(
+  Widget _buildBottomAppBar() {
+    return Container(
               padding: const EdgeInsets.all(8),
               color: Colors.white,
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  const Expanded(
+          Expanded(
                     child: TextField(
-                      decoration: InputDecoration(border: InputBorder.none),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: Dictiornary.typeMessage,
+                hintStyle: const TextStyle(color: Colors.grey),
+              ),
                     ),
                   ),
                   Transform.rotate(
                     angle: -30 * pi / 180,
                     child: IconButton(
                       onPressed: () {},
-                      icon: const Icon(Icons.send_rounded,
-                          color: AppColors.blue1),
+              icon: const Icon(Icons.send_rounded, color: AppColors.blue1),
                     ),
                   ),
                 ],
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
