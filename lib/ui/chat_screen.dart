fea6a0e47ab82da33b4e85ff2721305320416ef0
Shady <ShadyZekry75@gmail.com>
2022-05-17 23:29:35 +0200

    Add chat appbar

--- lib/ui/chat_screen.dart
+++ lib/ui/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:chillax/resources/colors.dart';
 import 'package:chillax/ui/widgets/chat_bubble.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +14,36 @@ class ChatScreen extends StatelessWidget {
         backgroundColor: AppColors.blue5,
         body: Column(
           children: [
-            ChatBubble(isMyMessage: true),
-            ChatBubble(isMyMessage: false),
+            Expanded(
+              child: Column(
+                children: const [
+                  ChatBubble(isMyMessage: true),
+                  ChatBubble(isMyMessage: false),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.white,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(border: InputBorder.none),
+                    ),
+                  ),
+                  Transform.rotate(
+                    angle: -30 * pi / 180,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.send_rounded,
+                          color: AppColors.blue1),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
